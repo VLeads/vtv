3e3e6e49230b06a458d7cfa0fbb483800490fb33
Vishal Kumar <leader.vishalkumar@gmail.com>
2021-05-08 18:22:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import Movie from './components/Movie';
+
+const FEATURED_API =  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a00888b1&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a00888b1&query=";
 
 function App() {
+  const [ movies, setMovies ] = useState([]);
+ 
+  useEffect(async() => {
+    const movieResp = await fetch(FEATURED_API);
+    const movieJson = await movieResp.json();
+    console.log(movieJson);
+    setMovies(movieJson);
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies.length > 0 && movies.map((movie) => (
+        <Movie />
+      )
+      )
+      }
     </div>
   );
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Movie= () => (
+    <div className="movie">This is a movie component !!</div>
+)
+
+export default Movie;
